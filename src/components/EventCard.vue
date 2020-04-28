29f244e3183743c1cf76518de745bf6e15ac447d
Shivelle <shivelle@gmx.net>
2020-04-28 11:50:23 +0200

    editor setup x3

---
+++ src/components/EventCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1 v-if @click="handler(arg, event)"></h1>
+</template>
+<script>
+export default {
+  import { libName } from 'libName';
+  props: {
+    propName: {
+      type: Number,
+      default: 
+    },
+  },
+  data() {
+    return {
+      key: value
+    }
+  }
+}
+</script>
+
+<style></style>
