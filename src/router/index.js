3d3c2d541bf970f620395c1e09fdc6fdf84d9736
Shivelle <shivelle@gmx.net>
2020-05-01 10:54:47 +0200

    user example for 'Dynamic Routing & History Mode'

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,10 @@ import VueRouter from 'vue-router'
 import EventCreate from '../views/EventCreate.vue'
 import EventList from '../views/EventList.vue'
 import EventShow from '../views/EventShow.vue'
+import User from '../views/User.vue'
+import NotFound from '../views/NotFound.vue'
 
 Vue.use(VueRouter)
-
 const routes = [
   {
     path: '/',
@@ -21,10 +22,22 @@ const routes = [
     path: '/event/create',
     name: 'event-create',
     component: EventCreate
+  },
+  {
+    path: '/user/:username',
+    name: 'user',
+    component: User,
+    props: true
+  },
+  {
+    // 404 are no longer being displayed with history mode in production as all requests lead trhough index.html
+    path: '*',
+    component: NotFound
   }
 ]
 
 const router = new VueRouter({
+  mode: 'history',
   routes
 })
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Oops, we can't find that page.</h1>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
