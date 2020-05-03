3ce894f8ac82423a335254e4a8f7c6218bff1abd
Shivelle <shivelle@gmx.net>
2020-05-03 13:36:20 +0200

    Single File Vue Components

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,21 +1,59 @@
 <template>
-  <h1 v-if @click="handler(arg, event)"></h1>
+  <router-link
+    class="event-link"
+    :to="{ name: 'event-show', params: { id: '1' } }"
+  >
+    <div class="event-card -shadow">
+      <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
+      <h4 class="title">{{ event.title }}</h4>
+      <span>{{ event.attendees.length }} attending</span>
+    </div>
+  </router-link>
 </template>
+
 <script>
 export default {
-  import { libName } from 'libName';
-  props: {
-    propName: {
-      type: Number,
-      default: 
-    },
-  },
   data() {
     return {
-      key: value
+      event: {
+        id: 1,
+        title: 'Beach Cleanup',
+        date: 'Tues Aug 20, 2020',
+        time: '6:00',
+        attendees: [
+          {
+            id: 'abc321',
+            name: 'Max Muster'
+          },
+          {
+            id: 'abc654',
+            name: 'Maria Muster'
+          }
+        ]
+      }
     }
   }
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.event-card {
+  padding: 20px;
+  margin-bottom: 24px;
+  transition: all 0.2s linear;
+  cursor: pointer;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
+}
+.event-card > .title {
+  margin: 0;
+}
+
+.event-link {
+  color: black;
+  text-decoration: none;
+  font-weight: 100;
+}
+</style>
